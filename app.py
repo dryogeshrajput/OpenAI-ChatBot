e59b15e33b6237a4e3406fcd041b1f31231c3644
Yogesh Rajput <94129924+dryogeshrajput@users.noreply.github.com>
2023-04-18 15:25:06 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+import openai
+import streamlit as st
+
+openai.api_key = "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"
+
+# Set the page configuration
+st.set_page_config(page_title="OpenAI Chatbot", page_icon=":robot_face:", layout="wide", initial_sidebar_state="expanded")
+
+st.title("OpenAI Chatbot :robot_face:")
+
+model = "text-davinci-003"
+user = st.text_input("Enter your question here:", "", key="user_input")
+
+if user.lower() == "":
+    st.write("Enter your question above")
+
+else:
+    with st.spinner(text='Thinking...'):
+        completion = openai.Completion.create(model=model,
+                                              prompt=user,
+                                              max_tokens=1024,
+                                              temperature=0.5,
+                                              n=1,
+                                              stop=None)
+    response = completion.choices[0].text
+    st.divider()  # Draws a horizontal rule
+    st.write("Answer :  ", response)
+    st.divider()  # Draws a horizontal rule
